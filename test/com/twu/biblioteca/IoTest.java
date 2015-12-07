085de3485d73bb5ba49ef57bb28fd383e627d9f0
may <wxm112@me.com>
2015-12-07 13:03:39 +1100

    Added checkout attribute in the test data.

--- test/com/twu/biblioteca/IoTest.java
+++ test/com/twu/biblioteca/IoTest.java
@@ -1,9 +1,8 @@
 package com.twu.biblioteca;
 
+import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
-import org.json.simple.JSONObject;
-
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -14,15 +13,18 @@ public class IoTest {
 
     private IO file;
 
-    @Before public void initialize(){
+    @Before
+    public void initialize(){
         file = new IO();
     }
 
     @Test
     public void fileReaderTest() throws Exception {
         JSONObject obj = new JSONObject();
+        obj.put("Checkout","No");
         obj.put("Author","Johanna Basford");
         obj.put("PublicationYear", "2015");
-        assertEquals(obj, file.fileReader("./testData.json").get("Secret Garden"));
+        assertEquals(obj, file.fileReader("./testData.json").get("SECRET GARDEN"));
     }
+
 }
