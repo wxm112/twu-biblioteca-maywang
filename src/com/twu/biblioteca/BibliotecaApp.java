8b790724a31819b43f010e462b36ed359417e31a
may <wxm112@me.com>
2015-12-08 17:02:50 +1100

    put all classes together to run the game.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,21 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    private UI ui;
+    private Message message = new Message();
+    private IO io = new IO();
+    private Librarian librarian = new Librarian("./data.json");
+
+    public BibliotecaApp() {
+        this.ui = new UI(librarian,message,io);
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp app = new BibliotecaApp();
+        app.message.printMessasge("Welcome");
+        app.ui.printMainMenu();
     }
+
+
 }
