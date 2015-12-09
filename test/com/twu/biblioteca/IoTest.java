65e7af8affe8da8da6446d336f97b0682ff851bb
may <wxm112@me.com>
2015-12-09 15:34:25 +1100

    Changed the checkout field as boolean, and fixed the IoTest for it.

--- test/com/twu/biblioteca/IoTest.java
+++ test/com/twu/biblioteca/IoTest.java
@@ -16,7 +16,7 @@ public class IoTest {
     @Test
     public void fileReaderTest() throws Exception {
         JSONObject obj = new JSONObject();
-        obj.put("Checkout", "No");
+        obj.put("Checkout", false);
         obj.put("Author", "Johanna Basford");
         obj.put("PublicationYear", "2015");
         assertEquals(obj, file.fileReader("./testData.json").get("SECRET GARDEN"));
@@ -26,14 +26,14 @@ public class IoTest {
     public void updateJasonFileTest() throws Exception {
         JSONObject obj = new JSONObject();
         JSONObject bookDetails = new JSONObject();
-        bookDetails.put("Checkout", "Yes");
+        bookDetails.put("Checkout", true);
         bookDetails.put("Author", "Xuemei");
         bookDetails.put("PublicationYear", "2000");
         obj.put("SECRET GARDEN", bookDetails);
         file.updateJasonFile(obj, "./testData.json");
 
         JSONObject checkoutStatus = (JSONObject) file.fileReader("./testData.json").get("SECRET GARDEN");
-        assertEquals("Yes", checkoutStatus.get("Checkout"));
+        assertEquals(true, checkoutStatus.get("Checkout"));
         assertEquals("Xuemei", checkoutStatus.get("Author"));
         assertEquals("2000", checkoutStatus.get("PublicationYear"));
     }
@@ -42,17 +42,17 @@ public class IoTest {
     public void recoverData() {
         JSONObject obj = new JSONObject();
         JSONObject bookDetail1 = new JSONObject();
-        bookDetail1.put("Checkout", "No");
+        bookDetail1.put("Checkout", false);
         bookDetail1.put("Author", "Johanna Basford");
         bookDetail1.put("PublicationYear", "2015");
         obj.put("SECRET GARDEN", bookDetail1);
         JSONObject bookDetail2 = new JSONObject();
-        bookDetail2.put("Checkout", "No");
+        bookDetail2.put("Checkout", false);
         bookDetail2.put("Author", "Warner Brothers");
         bookDetail2.put("PublicationYear", "2015");
         obj.put("HARRY POTTER COLOURING BOOK", bookDetail2);
         JSONObject bookDetail3 = new JSONObject();
-        bookDetail3.put("Checkout", "Yes");
+        bookDetail3.put("Checkout", true);
         bookDetail3.put("Author", "Drew Daywal");
         bookDetail3.put("PublicationYear", "2015");
         obj.put("THE DAY THE CRAYONS QUIT", bookDetail3);
